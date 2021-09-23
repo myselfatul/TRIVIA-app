265afbd6b1153feb4c9495ed97631e8a88581cdd
myselfatul <atulverma.verma4@gmail.com>
2021-09-23 22:29:26 +0530

    index file

---
+++ index.js
@@ -0,0 +1,28 @@
+// 'use strict'
+
+// const express = require('express')
+// const systemController = require('./controller/systemController')
+
+
+// const app = express()
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+
+// app.post('/login', systemController.login)
+
+// app.get('/ques1', systemController.question1)
+
+// app.get('/ques2', systemController.question2)
+
+// app.post('/summary', systemController.saveHistory)
+
+// app.get('/history', systemController.getHistory)
+
+
+// // server
+// const PORT = 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
